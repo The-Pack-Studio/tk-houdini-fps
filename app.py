c0ed2463221e778b98e0b151322c55850cd0338f
Donat <donat.vanb@yahoo.com>
2025-05-13 10:04:32 +0200

    Added callback (afterload event) that warns the user if the frame range is not uptodate (uses the frame range app)

--- app.py
+++ app.py
@@ -29,12 +29,26 @@ class TkHouFpsHandler(Application):
         """
         return True
 
+
+    def destroy_app(self):
+        """
+        Called when the app is unloaded/destroyed
+        """
+        self.log_debug("Destroying tk-houdini-fps app")
+
+        hou.hipFile.removeEventCallback(self.hou_callback)
+
+
+
     ###############################################################################################
     # implementation
 
     def hou_callback(self, event_type):
         if event_type == hou.hipFileEventType.AfterLoad:
             self.fps_scene_opened()
+            self.check_frame_range()
+
+
 
     def fps_scene_opened(self):
         # get shotgun project fps
@@ -62,3 +76,43 @@ class TkHouFpsHandler(Application):
 
 
             hou.setFps(project_fps)
+
+
+    def check_frame_range(self):
+        '''
+        Compare the frame range inside Houdini with the frame range of ShotGrid
+        '''
+
+        set_framerange_app = self.engine.apps.get("tk-multi-setframerange")
+        if not set_framerange_app:
+            return None
+
+
+        in_frame, out_frame, head_frame, tail_frame = set_framerange_app.get_frame_range_from_shotgun()
+
+        current_in, current_out, current_head, current_tail = set_framerange_app.get_current_frame_range()
+
+        # If no values or missing values in SG, skip
+        if (not head_frame 
+            or not tail_frame
+            or not in_frame
+            or not out_frame):
+
+            return None
+
+
+        if (current_in != in_frame or 
+            current_out != out_frame or
+            current_head != head_frame or
+            current_tail != tail_frame):
+
+            message_text = ("The frame range has been modified in ShotGrid\n"
+                            "since you last saved this file.\n\n"
+                            "Do you want to sync the frame range ?"
+                            )
+
+            message = hou.ui.displayMessage(message_text, buttons=("OK", "Cancel"), severity=hou.severityType.Warning)
+
+            if message == 0:
+                set_framerange_app.run_app()
+            
